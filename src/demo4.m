a773aa292913e34879c10929a065809bcc160ec2
Dimitris Papadimitriou <jim_pap@live.com>
2021-04-15 09:32:26 +0300

    saveasppm working

--- src/demo4.m
+++ src/demo4.m
@@ -1,12 +1,16 @@
+delete dip.ppm
 load march.mat
 RGB = bayer2rgb(x);
 RGBres=myresize(RGB,150,200,'linear');
-w=1/3;
-q = imagequant(RGB,w,w,w);
+w=1/8;
+q = imagequant(RGBres,w,w,w);
 d = imagedequant(q,w,w,w);
 
 saveasppm(q,'dip.ppm',1/w);
 
 figure
-subplot(1,2,1),imshow(RGB)
-subplot(1,2,2),imshow(d)
+%subplot(1,2,1),imshow(RGB)
+%subplot(1,2,2),imshow(d)
+imshow('dip.ppm')
+figure 
+imshow(d)
--- src/saveasppm.m
+++ src/saveasppm.m
@@ -1,11 +1,24 @@
 function saveasppm(x,filename,K)
-N=size(x,1);
-M=size(x,2);
+M=size(x,1);
+N=size(x,2);
 
-file=fopen(filename, 'w');
-fprintf(file, 'P6 %d %d %d\n', M, N, K);
+mat=zeros(3,M*N);
+
+for i=1:M
+    for j=1:N
+        mat(1,i*(N)+j)=x(i,j,1);
+        mat(2,i*(N)+j)=x(i,j,2);
+        mat(3,i*(N)+j)=x(i,j,3);
+    end
+end
+res = reshape(mat,1,[]);
+
+file=fopen(filename, 'a');
+fprintf(file, 'P6 %d %d %d\n', N, M, K-1);
+fwrite(file,res,'uint8');
+    
+
 fclose(file);
-data=
 
 end
 
